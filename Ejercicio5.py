acfddad9586c433994ea19117ec792945aefd36d
Byauric <150602030+Byauric@users.noreply.github.com>
2023-11-19 02:21:32 +0000

    subiendo ejercicio 5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -3,3 +3,4 @@
 num_shows = int(input("¿Cuántos shows musicales has visto en el último año? "))
 ha_visto_mas_de_tres = num_shows > 3
 print("¿Has visto más de 3 shows musicales? {}".format(ha_visto_mas_de_tres))
+
