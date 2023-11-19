88c2e760466985de5c2308efb15cc6b6fb9b6e19
Byauric <150602030+Byauric@users.noreply.github.com>
2023-11-19 02:18:36 +0000

    subiendo ejercicio 2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -3,4 +3,3 @@ costo_comida = float(input("Ingrese el monto de su consumo: S/"))
 porcentaje_propina = float(input("Ingrese el porcentaje de propina que desea dejar (debe ser mayor a 15%): "))
 propina = costo_comida * (porcentaje_propina / 100)
 print("La cantidad de propina a dejar es: S/{:.1f}".format(propina))
-
